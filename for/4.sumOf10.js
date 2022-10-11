48dc924e9d254059318d2e5a485fc804ffb74425
artemlozinski <artemlozinski@gmail.com>
2022-10-11 22:31:54 +0700

    0008

--- for/4.sumOf10.js
+++ for/4.sumOf10.js
@@ -2,12 +2,12 @@
 // Дано 10 целых чисел. Вычислите их сумму.
 // Напишите программу, использующую наименьшее число переменных.
 
-const a = []
-
-function sum(a) {
-    for (let i = a; i <= a; i + a[i]) {
-
-    }
-    
+const arr = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+function Sum(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  console.log(sum);
 }
-
+Sum(arr);
