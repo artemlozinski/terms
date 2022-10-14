415cfa80d5110171933f3682d8fea06e492626ab
artemlozinski <artemlozinski@gmail.com>
2022-10-14 14:30:31 +0700

    0009

--- for/1.line.js
+++ for/1.line.js
@@ -1,9 +1,8 @@
 // Ряд - 1
 // Даны два целых числа A и B (при этом A ≤ B). Выведите все числа от A до B включительно.
 
-const arr = []
-
 function line(a, b) {
+    const arr = []
     for (let i = a; i <= b; i++) {
         arr.push(i);
     }
